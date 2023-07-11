8cbd46cbb71e417a25136e69304321384ce7763f
Lukashevich Alesya <Lukasheviich03@gmail.com>
2023-07-11 19:00:53 +0300

    fixed 4 task

--- HW4,5/for.py
+++ HW4,5/for.py
@@ -1,18 +1,18 @@
 #  1
-def find_sum(a, b):
+def find_sum_int(a, b):
     """ Данная функция принимает следующие аргументы : a, b
     Выводит сумму всех целых чисел от а до б"""
     sum = 0
-    for i in range(a, b+1):
-        sum += i
+    for num in range(a, b + 1):
+        sum += num
     return sum
 
 
-print(find_sum(2, 4))
+print(find_sum_int(2, 4))
 
 
 #  2
-def find_sum1(x, y):
+def find_sum(x, y):
     """#2 находит сумму всех чисел в диапазоне между двумя заданными натуральными числами"""
     total = 0
     if x >= 0 and y >= 0:
@@ -23,55 +23,53 @@ def find_sum1(x, y):
         pass
 
 
-print(find_sum1(1, 5))
+print(find_sum(1, 5))
 
 
 #  3
-def nums(a):
+def nums(num):
     """ #3 возвращает значение произведения положительных, сумму и количество отрицательных
         из 10 введенных целых значений
     """
-    numer = 1
-    s = 0
-    t = 0
-    for i in a:
+    multiply_of_variables = 1
+    count_negative_num = 0
+    sum_negative_num = 0
+    for i in num:
         if i > 0:
-            numer *= i
+            multiply_of_variables *= i
         if i < 0:
-            s += 1
-            t += i
-    return numer, s, t
+            count_negative_num += 1
+            sum_negative_num += i
+    return multiply_of_variables, count_negative_num, sum_negative_num
 
 
-a = [int(x) for x in input('Введите числа через пробел: ').split(' ')]
+num = [int(x) for x in input('Введите числа через пробел: ').split(' ')]
 
-
-print(nums(a))
+print(nums(num))
 
 
 #  4
-def best_result(**kwargs):
-    """ # 4 выводит лучший результат среди 6 участников"""
-    best_result1 = min(kwargs.values())
-    for key, value in kwargs.items():
-        if value == best_result1:
-            print(best_result1)
-            return key, value
+def find_best_swimmer():
+    """
+     # 4 выводит лучший результат среди 6 участников
+    """
+    swimmers = {"Бекиш Александр": 21.07,
+                "Будник Алексей": 20.34,
+                "Гребень Анастасия": 22.12,
+                "Давидович Татьяна": 30,
+                "Дешук Дмитрий": 24.01,
+                "Казак Анна": 28.17}
+    return min(swimmers, key=swimmers.get)
 
 
-best_result(Бекиш_Александр='21.07',
-            Будник_Алексей='20.34',
-            Гребень_Анастасия='22.12',
-            Давидович_Татьяна='30',
-            Дешук_Дмитрий='24.01',
-            Казак_Анна='28.17')
+print(find_best_swimmer())
 
 
 #  5
-def output_a_unique_number(list_4):
+def output_a_unique_number(list_of_numbers):
     """ #5 Возвращает из списка цифру встречающуюся единожды"""
-    for i in list_4:
-        if list_4.count(i) == 1:
+    for i in list_of_numbers:
+        if list_of_numbers.count(i) == 1:
             return i
 
 
